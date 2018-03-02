16252c4c695ef69c16a43269257b94cd204509ee
Mike Murphy <michael.j.murphy@uit.no>
2018-03-02 20:54:49 +0100

    Basic Python HTTP server

---
+++ server.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env/python3
+
+# Basic Python HTTP server with threading and shutdown.
+
+import argparse
+import http.server
+import logging
+import re
+import signal
+import socket
+import socketserver
+import sys
+import threading
+
+def parse_args():
+    PORT_DEFAULT = 8000
+    DIE_AFTER_SECONDS_DEFAULT = 20 * 60
+    SHUTDOWN_GRACE_PERIOD_DEFAULT = 2
+    LOG_LEVEL_DEFAULT = "INFO"
+
+    parser = argparse.ArgumentParser(prog=__file__)
+
+    parser.add_argument("-p", "--port", type=int, default=PORT_DEFAULT,
+            help="port number to listen on, default %d" % PORT_DEFAULT)
+
+    parser.add_argument("--die-after-seconds", type=float,
+            default=DIE_AFTER_SECONDS_DEFAULT,
+            help="kill server after so many seconds have elapsed, " +
+                "in case we forget or fail to kill it, " +
+                "default %d (%d minutes)" % (DIE_AFTER_SECONDS_DEFAULT, DIE_AFTER_SECONDS_DEFAULT/60))
+
+    parser.add_argument("--shutdown-grace-period", type=float,
+            default=SHUTDOWN_GRACE_PERIOD_DEFAULT,
+            help="When server is asked to shutdown, give it this many seconds to shutdown cleanly. Default: {}".format(SHUTDOWN_GRACE_PERIOD_DEFAULT))
+
+    parser.add_argument("--loglevel", default=LOG_LEVEL_DEFAULT,
+            help="Logging level. ERROR, WARN, INFO, DEBUG. Default: {}".format(LOG_LEVEL_DEFAULT))
+
+    return parser.parse_args()
+
+
+class ThreadingHttpServer(http.server.HTTPServer, socketserver.ThreadingMixIn):
+    pass
+
+class HttpRequestHandler(http.server.BaseHTTPRequestHandler):
+    pass
+
+
+def run_http_server(args):
+    server = ThreadingHttpServer( ('', args.port), HttpRequestHandler)
+
+    logging.basicConfig(level=args.loglevel)
+
+    def run_server():
+        logging.info("Starting server on port %d." , args.port)
+        server.serve_forever()
+        logging.info("Server has shut down cleanly.")
+
+    # Start HTTP server in a separate thread for proper shutdown
+    #
+    # serve_forever() and shutdown() must be called from separate threads
+    # for the shutdown to work properly.
+    # shutdown() is called by signal handlers and by the timeout,
+    # which both execute on the main thread.
+    # So the server must be running on a separate thread.
+    thread = threading.Thread(target=run_server)
+    # Setting thread as daemon will allow the program to exit even if the server gets hung up.
+    thread.daemon = True
+    thread.start()
+
+    def shutdown_server_with_grace_period():
+        logging.info("Asking server to shut down.")
+        server.shutdown()
+        thread.join(args.shutdown_grace_period)
+        if thread.isAlive():
+            logging.error("Server thread is still alive after %.3f-second grace period. Trying to exit anyway.", args.shutdown_grace_period)
+            sys.exit(1)
+
+    def shutdown_server_on_signal(signum, frame):
+        if hasattr(signal, "Signals"):
+            signame = signal.Signals(signum).name
+            sigdesc = "{}, {}".format(signum, signame)
+        else:
+            sigdesc = str(signum)
+        logging.info("Got system signal %s.", sigdesc)
+        shutdown_server_with_grace_period()
+
+    # Install signal handlers
+    signal.signal(signal.SIGTERM, shutdown_server_on_signal)
+    signal.signal(signal.SIGINT, shutdown_server_on_signal)
+
+    # Run until given timeout
+    thread.join(args.die_after_seconds)
+
+    # Check if server timed out instead of exiting
+    if thread.isAlive():
+        logging.warn("Reached %.3f second timeout.", args.die_after_seconds)
+        shutdown_server_with_grace_period()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_http_server(args)
