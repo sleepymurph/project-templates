b9bbd65318fa907ac30844b36dec5bc75d96e5ed
Mike Murphy <michael.j.murphy@uit.no>
2018-03-19 12:23:23 +0100

    Refactor tmux session for easier customization

--- latex_report/tmux-session-doc.sh
+++ latex_report/tmux-session-doc.sh
@@ -12,24 +12,31 @@ browser() {
 
 SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
+# ↓↓↓ Set project-specific variables here ↓↓↓
 
 SESSION_NAME="latex"
 WINDOW_NAME="latex"
 REPO_URL="https://github.com/sleepymurph/project-templates"
 
+# ↑↑↑ --- ↑↑↑
+
 # Set start directory for whole session
 cd "$SCRIPT_DIR"
-
-# Lauch web pages and documents
-browser_new_window "$REPO_URL"
-xdg-open "doc.pdf"
 
 # start tmux session
 tmux new-session -d -s "$SESSION_NAME" -n "$WINDOW_NAME"
 tmux select-window -t "$SESSION_NAME:0"
 
+# Launch web pages and documents
+browser_new_window "$REPO_URL"
+xdg-open "doc.pdf"
+
+# ↓↓↓ Customize here ↓↓↓
+
 # In first window
 tmux send-keys 'vim -O doc-content.tex sources.bib' C-m
+
+# ↑↑↑ --- ↑↑↑
 
 # back to the beginning
 tmux select-window -t ':0'
