7156c07c530704db0e084323676e7dd502d9e1a9
Mike Murphy <michael.j.murphy@uit.no>
2018-04-23 15:24:36 +0200

    Allow deeper nesting of latex source dir in repo

--- latex-article/docker_make.sh
+++ latex-article/docker_make.sh
@@ -5,17 +5,17 @@
 set -x
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}" | xargs readlink -f)"
-SCRIPT_DIR_NAME="$(basename "$SCRIPT_DIR")"
-PARENT_DIR="$(echo "$SCRIPT_DIR/.." | xargs readlink -f)"
+GIT_ROOT_DIR="$(cd $SCRIPT_DIR && git rev-parse --show-toplevel)"
 
-IMAGE_NAME="$SCRIPT_DIR_NAME"
+SCRIPT_PATH_FROM_GIT_ROOT="${SCRIPT_DIR##$GIT_ROOT_DIR}"
+IMAGE_NAME="$(basename "$SCRIPT_DIR")"
 
 docker build --tag "$IMAGE_NAME" "$SCRIPT_DIR"
 
 docker run \
     --rm \
-    --volume "$PARENT_DIR":/home/compile/repo \
-    --workdir "/home/compile/repo/$SCRIPT_DIR_NAME" \
+    --volume "$GIT_ROOT_DIR":/home/compile/repo \
+    --workdir "/home/compile/repo/$SCRIPT_PATH_FROM_GIT_ROOT" \
     --env UID=$(id -u) \
     "$IMAGE_NAME" \
     make $*
